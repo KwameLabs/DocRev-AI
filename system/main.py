ce7b3c30b120f13ce0a97b1dce0b749da3a8e3be
KwameLabs <michael.larbi@live.com>
2024-10-17 15:40:52 +0900

    Update main.py

--- system/main.py
+++ system/main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle as pk
 import pandas as pd
-import plotly.graph_objects as go
+#import plotly.graph_objects as go
 import numpy as np
 import os
 import tempfile
