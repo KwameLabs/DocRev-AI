291c13e23d079a8c122a90c20e45cd2f89f8a756
KwameLabs <michael.larbi@live.com>
2024-11-01 01:01:31 +0900

    Update review.py

--- reviewer/review.py
+++ reviewer/review.py
@@ -8,7 +8,7 @@ import os
 import tempfile
 import shutil
 import logging
-from apikey import apikey
+
 from langchain_community.embeddings.openai import OpenAIEmbeddings
 from langchain.llms import openai
 from langchain.prompts import PromptTemplate
@@ -61,7 +61,7 @@ warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
 
 #########Initialiasation#################
-os.environ['OPENAI_API_KEY'] = apikey
+os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
 ROOT_DIR = os.path.abspath(os.curdir)
 #llm = openai.OpenAI(temperature=0)
 llm = ChatOpenAI(model="gpt-4",temperature=1)
